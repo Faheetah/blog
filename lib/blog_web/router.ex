ed7c00dc0ca2b3c63a0eb7be138bdba364e886b1
Faheetah <Faheetah@users.noreply.github.com>
2022-05-03 18:41:07 -0500

    mix phx.new --no-gettext --no-dashboard --no-live --no-mailer blog

---
+++ lib/blog_web/router.ex
@@ -0,0 +1,27 @@
+defmodule BlogWeb.Router do
+  use BlogWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, {BlogWeb.LayoutView, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", BlogWeb do
+    pipe_through :browser
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", BlogWeb do
+  #   pipe_through :api
+  # end
+end
