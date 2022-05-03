fa0ad805e2572b540af8b2348194fa8962e0eb64
Faheetah <Faheetah@users.noreply.github.com>
2022-05-03 18:48:31 -0500

    Add tailwind

---
+++ assets/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: "jit",
+  content: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
